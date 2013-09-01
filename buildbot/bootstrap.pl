bbf57e5a15362ad8442c8ef91b77eb2eb0a2e642
Dr!fter <Dr!fter@localhost>
2013-09-01 11:11:38 -0400

    Add AMBuild

---
+++ buildbot/bootstrap.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+# vim: set ts=2 sw=2 tw=99 noet: 
+
+use strict;
+use Cwd;
+use File::Basename;
+use File::Path;
+
+my ($myself, $path) = fileparse($0);
+chdir($path);
+
+require 'helpers.pm';
+
+#Go back above build dir
+chdir(Build::PathFormat('../..'));
+
+#Get the source path.
+our ($root) = getcwd();
+
+rmtree('OUTPUT');
+mkdir('OUTPUT') or die("Failed to create output folder: $!\n");
+chdir('OUTPUT');
+my ($result);
+print "Attempting to reconfigure...\n";
+
+#update and configure shiz
+if ($^O eq "linux") {
+	#my @sdks = ('sourcemod-1.3', 'mmsource-1.8', 'hl2sdk', 'hl2sdk-ob', 'hl2sdk-ob-valve', 'hl2sdk-l4d', 'hl2sdk-l4d2');
+	#my ($sdk);
+	#foreach $sdk (@sdks) {
+	#	print "Updating checkout of ", $sdk, " on ", $^O, "\n";
+	#	$result = `hg pull -u /home/builds/common/$sdk`;
+	#	print $result;
+	#}
+	
+	$ENV{'SOURCEMOD14'} = '/home/builds/common/sourcemod-1.4';
+	$ENV{'MMSOURCE19'} = '/home/builds/common/mmsource-1.9';
+	
+	$ENV{'HL2SDK'} = '/home/builds/common/hl2sdk';
+	$ENV{'HL2SDKOB'} = '/home/builds/common/hl2sdk-ob';
+	$ENV{'HL2SDKOBVALVE'} = '/home/builds/common/hl2sdk-ob-valve';
+	$ENV{'HL2SDKL4D'} = '/home/builds/common/hl2sdk-l4d';
+	$ENV{'HL2SDKL4D2'} = '/home/builds/common/hl2sdk-l4d2';
+	$ENV{'HL2SDKCSGO'} = '/home/builds/common/hl2sdk-csgo';
+} elsif ($^O eq "darwin") {
+	#my @sdks = ('sourcemod-1.3', 'mmsource-1.8', 'hl2sdk-ob-valve');
+	#my ($sdk);
+	#foreach $sdk (@sdks) {
+	#	print "Updating checkout of ", $sdk, " on ", $^O, "\n";
+	#	$result = `hg pull -u /Users/builds/slaves/common/$sdk`;
+	#	print $result;
+	#}
+	
+	$ENV{'SOURCEMOD14'} = '/Users/builds/slaves/common/sourcemod-1.4';
+	$ENV{'MMSOURCE18'} = '/Users/builds/slaves/common/mmsource-1.8';
+	$ENV{'MMSOURCE19'} = '/Users/builds/slaves/common/mmsource-central';
+	
+	$ENV{'HL2SDKOBVALVE'} = '/Users/builds/slaves/common/hl2sdk-ob-valve';
+	$ENV{'HL2SDKCSS'} = '/Users/builds/slaves/common/hl2sdk-css';
+} else {
+	#JUST IN CASE
+	#$ENV{'MMSOURCE19'} = "C:\\Scripts\\common\\mmsource-1.9";
+	#$ENV{'HL2SDKCSGO'} = "C:\\Scripts\\common\\hl2sdk-csgo";
+}
+# Windows vars should already be set.
+
+#configure AMBuild
+if ($^O eq "linux") {
+	$result = `CC=gcc CXX=gcc python3 ../build/configure.py --enable-optimize`;
+} elsif ($^O eq "darwin") {
+	$result = `CC=clang CXX=clang python3 ../build/configure.py --enable-optimize`;
+} else {
+	$result = `C:\\Python31\\Python.exe ..\\build\\configure.py --enable-optimize`;
+}
+print "$result\n";
+if ($? != 0) {
+	die('Could not configure!');
+}
--- vfunc_call.h
+++ vfunc_call.h
@@ -132,7 +132,85 @@ T CallVFunction(DHooksCallback *dg, HookParamsStruct *paramStruct, void *iface)
 
 	return ret;
 }
+template <>
+Vector CallVFunction<Vector>(DHooksCallback *dg, HookParamsStruct *paramStruct, void *iface)
+{
+	SourceMod::PassInfo *paramInfo = NULL;
+	SourceMod::PassInfo returnInfo;
 
+	if(dg->returnType != ReturnType_Void)
+	{
+		returnInfo.flags = dg->returnFlag;
+		returnInfo.size = sizeof(Vector);
+		returnInfo.type = PassType_Object;
+	}
+
+	ICallWrapper *pCall;
+
+	size_t size = GetStackArgsSize(dg);
+
+	unsigned char *vstk = (unsigned char *)malloc(sizeof(void *) + size);
+	unsigned char *vptr = vstk;
+
+	*(void **)vptr = iface;
+
+	if(paramStruct)
+	{
+		vptr += sizeof(void *);
+		paramInfo = (SourceMod::PassInfo *)malloc(sizeof(SourceMod::PassInfo) * dg->params.Count());
+		for(int i = 0; i < dg->params.Count(); i++)
+		{
+			switch(dg->params.Element(i).type)
+			{
+				case HookParamType_Int:
+					PARAMINFO_SWITCH(PassType_Basic);
+					VSTK_PARAM_SWITCH(int);
+				case HookParamType_Bool:
+					PARAMINFO_SWITCH(PassType_Basic);
+					VSTK_PARAM_SWITCH(cell_t);
+				case HookParamType_Float:
+					PARAMINFO_SWITCH(PassType_Float);
+					VSTK_PARAM_SWITCH_FLOAT();
+				case HookParamType_String:
+					PARAMINFO_SWITCH(PassType_Object);
+					VSTK_PARAM_SWITCH(int);
+				case HookParamType_StringPtr:
+					PARAMINFO_SWITCH(PassType_Basic);
+					VSTK_PARAM_SWITCH(string_t *);
+				case HookParamType_CharPtr:
+					PARAMINFO_SWITCH(PassType_Basic);
+					VSTK_PARAM_SWITCH(char *);
+				case HookParamType_VectorPtr:
+					PARAMINFO_SWITCH(PassType_Basic);
+					VSTK_PARAM_SWITCH(Vector *);
+				case HookParamType_CBaseEntity:
+					PARAMINFO_SWITCH(PassType_Basic);
+					VSTK_PARAM_SWITCH(CBaseEntity *);
+				case HookParamType_Edict:
+					PARAMINFO_SWITCH(PassType_Basic);
+					VSTK_PARAM_SWITCH(edict_t *);
+				default:
+					PARAMINFO_SWITCH(PassType_Basic);
+					VSTK_PARAM_SWITCH(void *);
+			}
+		}
+	}
+
+	Vector ret;
+
+	pCall = g_pBinTools->CreateVCall(dg->offset, 0, 0, &returnInfo, paramInfo, dg->params.Count());
+	pCall->Execute(vstk, &ret);
+
+	pCall->Destroy();
+	free(vstk);
+
+	if(paramInfo != NULL)
+	{
+		free(paramInfo);
+	}
+
+	return ret;
+}
 #ifdef __linux__
 template <>
 string_t CallVFunction<string_t>(DHooksCallback *dg, HookParamsStruct *paramStruct, void *iface)
@@ -198,18 +276,10 @@ string_t CallVFunction<string_t>(DHooksCallback *dg, HookParamsStruct *paramStru
 		}
 	}
 
-	string_t ret = NULL_STRING;
-	
-	if(dg->returnType == ReturnType_Void)
-	{
-		pCall = g_pBinTools->CreateVCall(dg->offset, 0, 0, NULL, paramInfo, dg->params.Count());
-		pCall->Execute(vstk, NULL);
-	}
-	else
-	{
-		pCall = g_pBinTools->CreateVCall(dg->offset, 0, 0, &returnInfo, paramInfo, dg->params.Count());
-		pCall->Execute(vstk, &ret);
-	}
+	string_t ret;
+
+	pCall = g_pBinTools->CreateVCall(dg->offset, 0, 0, &returnInfo, paramInfo, dg->params.Count());
+	pCall->Execute(vstk, &ret);
 
 	pCall->Destroy();
 	free(vstk);
