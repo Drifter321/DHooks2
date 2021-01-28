19d6b32cde610a9437f94c70de335615b10194b0
Peace-Maker <peace-maker@wcfan.de>
2021-01-28 09:25:16 +0100

    Bump version to v2.2.0-detours16
    Also update the author list in the extension info.

--- sdk/smsdk_config.h
+++ sdk/smsdk_config.h
@@ -40,8 +40,8 @@
 /* Basic information exposed publicly */
 #define SMEXT_CONF_NAME			"DHooks"
 #define SMEXT_CONF_DESCRIPTION	"Dynamic Hooks"
-#define SMEXT_CONF_VERSION		"2.2.0-detours15"
-#define SMEXT_CONF_AUTHOR		"Dr!fter"
+#define SMEXT_CONF_VERSION		"2.2.0-detours16"
+#define SMEXT_CONF_AUTHOR		"Dr!fter and Peace-Maker"
 #define SMEXT_CONF_URL			"http://www.sourcemod.net/"
 #define SMEXT_CONF_LOGTAG		"DHOOKS"
 #define SMEXT_CONF_LICENSE		"GPL"
